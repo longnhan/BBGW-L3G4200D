84d058643bc1e403aa68a03286eebaa29adf1ee6
Nhan <longnhan252@gmail.com>
2024-02-24 00:02:36 +0700

    thread to update raw data

--- src/main.c
+++ src/main.c
@@ -14,6 +14,10 @@ char i2c_Device[]="/dev/i2c-2";
 uint8_t device_Address = 0x69;
 enum deviceOperationMode device_Mode = Device_Mode_Normal;
 
+int16_t xAxis = 0;
+int16_t yAxis = 0; 
+int16_t zAxis = 0;
+
 int main(void)
 {
     /*thread declare*/
@@ -22,13 +26,11 @@ int main(void)
     /*variables init*/
     uint8_t buf[1]={0};
     int8_t temp=0;
-    int16_t xAxis = 0;
-    int16_t yAxis = 0; 
-    int16_t zAxis = 0;
 
     /*function declare*/
     void *createFile(void *ptr);
     void *systemInit(void *ptr);
+    void *readSensorData(void *ptr);
     
     //Handle thread
     pthread_create(&thrd_Create_File, NULL, createFile, NULL);
@@ -43,10 +45,8 @@ int main(void)
         printf("device temperature: %d\n", temp);
         readDeviceName(buf);
         printf("device name: 0x%x\n", buf[0]);
-        read_X_Axis(&xAxis);
-        read_Y_Axis(&yAxis);
-        read_Z_Axis(&zAxis);
-        printf("X axis: %d |Y axis: %d|Z axis: %d\n",xAxis, yAxis, zAxis);
+        pthread_create(&thrd_Read_Data, NULL, readSensorData, NULL);
+        pthread_join(thrd_Read_Data, NULL);       
     }
     return 0;    
 }
@@ -76,4 +76,12 @@ void *systemInit(void *ptr)
     
     setDeviceMode(device_Mode);
     return NULL;
+}
+
+void *readSensorData(void *ptr)
+{
+    read_X_Axis(&xAxis);
+    read_Y_Axis(&yAxis);
+    read_Z_Axis(&zAxis);
+    printf("X axis: %d |Y axis: %d|Z axis: %d\n",xAxis, yAxis, zAxis);
 }
