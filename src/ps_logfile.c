bdd8e30777fa11f440065c21d09bc4a1d62922df
nhan <longnhan252@gmail.com>
2024-05-21 16:00:08 +0700

    basic communication between 2 seperatly processes
    sending and receiving by mesg queue
    
    setup signal notify new available msg in queue
    
    log file

--- src/ps_logfile.c
+++ src/ps_logfile.c
@@ -1,7 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
 #include <stdint.h>
 #include <fcntl.h>
@@ -10,6 +9,15 @@
 #include <signal.h>
 
 #define MQ_NAME         "/myQueue"
+#define MQ_QUEUE_SIZE        10
+#define MQ_MSG_SIZE         1024
+
+/*message queue*/
+mqd_t mq_gyro;
+/*message queue notification*/
+struct sigevent mqSignal;
+/*mesage queue attributes*/
+struct mq_attr mq_gyro_attr;
 
 /*pointer log file*/
 FILE *fp;
@@ -21,39 +29,57 @@ int16_t zAxis = 0;
 uint8_t device_name = 0;
 /*sensor temperature*/
 int8_t temp=0;
+/*data sensor to send queue*/
+char mqSensorBuffer[MQ_MSG_SIZE]={0};
+
+/*Function declaration*/
+void *createFile(void *ptr);
+void *logData(void *ptr);
+void signalHandler(int sig);
 
 int main()
 {
-    /*function declare*/
+    /*msg queue attributes*/
+    mq_gyro_attr.mq_flags = 0;
+    mq_gyro_attr.mq_maxmsg = MQ_QUEUE_SIZE;
+    mq_gyro_attr.mq_msgsize = MQ_MSG_SIZE;
+    mq_gyro_attr.mq_curmsgs = 0;
 
-    /*message queue*/
-    mqd_t mq_gyro;
-
-    mq_gyro = mq_open(MQ_NAME, O_RDONLY, 0666, NULL);
+    /*message queue creating*/
+    mq_gyro = mq_open(MQ_NAME, O_RDONLY, 0644, &mq_gyro_attr);
     if(mq_gyro == -1)
     {
         printf("msg queue create fail\n");
+        perror("mq_open");
         exit(EXIT_FAILURE);
     }
+    else
+    {
+        printf("queue opened successfully\n");
+    }
 
-    /*msg queue notification*/
-    struct sigevent mqSignal;
+    /*setup message queue notification*/
     mqSignal.sigev_notify = SIGEV_SIGNAL;
     mqSignal.sigev_signo = SIGUSR1;
 
     if(mq_notify(mq_gyro, &mqSignal) == -1)
     {
-        printf("msg queue notify create fail\n");
+        printf("msg queue notify SIGUSR1 create fail\n");
+        perror("mq_notify");
         mq_close(mq_gyro);
         exit(EXIT_FAILURE);
     }
+    else
+    {
+        printf("queue notify registered successfully\n");
+    }
+
+    /*register signal catching*/
+    signal(SIGUSR1, signalHandler);
+    signal(SIGINT, signalHandler);
 
     /*thread declare*/
     pthread_t thrd_Create_File, thrd_Log_Data;
-
-    /*function declare*/
-    void *createFile(void *ptr);
-    void *logData(void *ptr);
 
     /*Handle threads*/
     pthread_create(&thrd_Create_File, NULL, createFile, NULL);
@@ -62,6 +88,10 @@ int main()
     pthread_create(&thrd_Log_Data, NULL, logData, NULL);
     pthread_join(thrd_Log_Data, NULL);
     
+    /* Close the message queue */
+    mq_close(mq_gyro);
+    mq_unlink(MQ_NAME);
+
     return 0;
 }
 
@@ -71,6 +101,7 @@ void *createFile(void *ptr)
     if(fp == NULL)
     {
         printf("Create file fail\n");
+        exit(EXIT_FAILURE);
     }
     else
     {
@@ -80,29 +111,69 @@ void *createFile(void *ptr)
 }
 
 void *logData(void *ptr)
-{
-    /*function declare*/
-    void signalHandler(void);
-    
+{   
     static uint8_t count  = 1;
     while(1)
     {
         if(count == 1)
         {
-            fprintf(fp, "Read data from sensor L3G4200D\n");
+            fprintf(fp, "Log file read data from sensor L3G4200D\n");
             fprintf(fp, "Device name: 0x%x\n", device_name);
             fprintf(fp, "data x,data y,data z, temperature\n");
             count = 0;
         }
-        fprintf(fp, "%d,%d,%d,%d\n", xAxis, yAxis, zAxis, temp);
-        /*signal catching*/
-        signal(SIGUSR1, signalHandler);
-        usleep(100000);
+        
+        printf("Waiting for messages...\n");
+        pause();
     }
+    mq_close(mq_gyro);
+    mq_unlink(MQ_NAME);
+    
     return NULL;
 }
 
-void signalHandler(void)
+void signalHandler(int sig)
 {
-    /*re-register the notification*/
+    printf("get signal for new message queue\n");
+    if(sig == SIGUSR1)
+    {
+        /*read sensor data from queue*/
+        if(mq_receive(mq_gyro, mqSensorBuffer, MQ_MSG_SIZE, NULL) == -1)
+        {
+            printf("queue reading error\n");
+        }
+        else
+        {
+            /*copy data from buffer to varibles*/
+            memcpy(&xAxis, mqSensorBuffer, sizeof(int16_t));
+            memcpy(&yAxis, mqSensorBuffer + 1*sizeof(int16_t), sizeof(int16_t));
+            memcpy(&zAxis, mqSensorBuffer + 2*sizeof(int16_t), sizeof(int16_t));
+            memcpy(&temp, mqSensorBuffer + 3*sizeof(int16_t), sizeof(int8_t));
+
+            /*write data to log file*/
+            fprintf(fp, "%d,%d,%d,%d\n", xAxis, yAxis, zAxis, temp);
+            
+            printf("receive buffer DONE\n");
+            
+            /*setup message queue notification*/
+            mqSignal.sigev_notify = SIGEV_SIGNAL;
+            mqSignal.sigev_signo = SIGUSR1;
+
+            if(mq_notify(mq_gyro, &mqSignal) == -1)
+            {
+                printf("msg queue notify SIGUSR1 create fail\n");
+                mq_close(mq_gyro);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    else if(sig == SIGINT)
+    {
+        printf("\nCaught SIGINT, cleaning up...\n");
+        mq_close(mq_gyro);
+        mq_unlink(MQ_NAME);
+        fclose(fp);
+        exit(EXIT_SUCCESS);
+    }
+    
 }
--- src/ps_main.c
+++ src/ps_main.c
@@ -2,11 +2,6 @@
 #include "l3g4.h"
 #include "i2c_driver.h"
 
-/*function declare*/
-void *systemInit(void *ptr);
-void *readSensorData(void *ptr);
-void signalHandler(int sig);
-
 /*system i2c config*/
 int i2c_File;
 char i2c_Device[]="/dev/i2c-2";
@@ -15,6 +10,8 @@ enum deviceOperationMode device_Mode = Device_Mode_Normal;
 
 /*message queue*/
 mqd_t mq_gyro;
+/*msg queue attributes*/
+struct mq_attr mq_gyro_attr;
 
 /*sensor raw data*/
 int16_t xAxis = 0;
@@ -25,20 +22,20 @@ uint8_t device_name = 0;
 /*sensor temperature*/
 int8_t temp=0;
 /*data sensor to send queue*/
-int16_t mqSensorBuffer[5]={0};
+char mqSensorBuffer[MQ_MSG_SIZE]={0};
+char str_test[MQ_MSG_SIZE] = {"Hella"};
 
 int main(void)
 {
     printf("start opening msg queue\n");
     /*msg queue attributes*/
-    struct mq_attr mq_gyro_attr;
     mq_gyro_attr.mq_flags = 0;
     mq_gyro_attr.mq_maxmsg = MQ_QUEUE_SIZE;
     mq_gyro_attr.mq_msgsize = MQ_MSG_SIZE;
     mq_gyro_attr.mq_curmsgs = 0;
     
     /*open message queue*/
-    mq_gyro = mq_open(MQ_NAME, O_CREAT | O_WRONLY | O_NONBLOCK, 0644, &mq_gyro_attr);
+    mq_gyro = mq_open(MQ_NAME, O_CREAT | O_WRONLY, 0644, &mq_gyro_attr);
     if(mq_gyro == -1)
     {
         printf("msg queue create fail\n");
@@ -84,26 +81,31 @@ void *systemInit(void *ptr)
 
 void *readSensorData(void *ptr)
 {
+    printf("Type anything to start: "); //test
+    scanf("%s", str_test); //test
     while (1)
     {
+        /*get data from sensor*/
         read_X_Axis(&xAxis);
         read_Y_Axis(&yAxis);
         read_Z_Axis(&zAxis);
         readTemperature(&temp);
         readDeviceName(&device_name);
         
+        /*print out the sensor's data*/
+        printf("\n|-----------------------------------------------------|\n");
         printf("device temperature: %d\n", temp);
         printf("device name: 0x%x\n", device_name);
         printf("X axis: %d |Y axis: %d|Z axis: %d\n",xAxis, yAxis, zAxis);
 
-        mqSensorBuffer[0] = xAxis;
-        mqSensorBuffer[1] = yAxis;
-        mqSensorBuffer[2] = zAxis;
-        mqSensorBuffer[3] = temp;
-        mqSensorBuffer[4] = device_name;
+        /*copy sensor value to buffer*/
+        memcpy(mqSensorBuffer, &xAxis, sizeof(int16_t));
+        memcpy(mqSensorBuffer + 1*sizeof(int16_t), &yAxis, sizeof(int16_t));
+        memcpy(mqSensorBuffer + 2*sizeof(int16_t), &zAxis, sizeof(int16_t));
+        memcpy(mqSensorBuffer + 3*sizeof(int16_t), &temp, sizeof(int16_t));
 
         /*sending buffer to queue*/
-        if(mq_send(mq_gyro, (char *)mqSensorBuffer, strlen(mqSensorBuffer), 0) == -1)
+        if(mq_send(mq_gyro, mqSensorBuffer, MQ_MSG_SIZE, 0) == -1)
         {
             printf("queue sending error\n");
             perror("mq_send");
@@ -111,10 +113,16 @@ void *readSensorData(void *ptr)
         else
         {
             printf("queue sending successfully\n");
+            /*print out the number message*/
+            mq_getattr(mq_gyro, &mq_gyro_attr);
+            printf("current messgae in queue: %d\n", mq_gyro_attr.mq_curmsgs);
         }
 
-        usleep(10000);
+        usleep(100000);
     }
+    mq_close(mq_gyro);
+    mq_unlink(MQ_NAME);
+
     return NULL;
 }
 
@@ -129,6 +137,19 @@ void signalHandler(int sig)
     }
     else
     {
+
+    }
+}
+
+int dataToBuffer(char *ptr, uint16_t data)
+{
+    if(ptr == NULL)
+    {
         
     }
+    else
+    {
+
+    }
+    return 0;
 }
